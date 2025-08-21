edbdaa37cc388ce8a253273954bf01f442a36662
Bikram Keshari Dash <bikramkesharidash7077@gmail.com>
2025-08-22 01:42:16 +0530

    Add files via upload

---
+++ OddSequence.java
@@ -0,0 +1,21 @@
+package OddSeries;
+import java.util.Scanner;
+
+public class OddSequence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number : ");
+        int a = sc.nextInt();
+
+       
+        for (int i = 0; i < a; i++) {   
+            int odd = 2 * i + 1;       
+            System.out.print(odd);
+            if (i < a - 1) {            
+                System.out.print(", ");
+            }
+        }
+    }
+}
+
